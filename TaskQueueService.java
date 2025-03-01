3f4ddb35b1fdde7ca59835067015cec4f3a409a1
Maxxomade <maxxomadeit0@gmail.com>
2025-02-28 20:35:14 -0600

    Update TaskQueueService.java

--- TaskQueueService.java
+++ TaskQueueService.java
@@ -1,1 +1,22 @@
+// service/TaskQueueService.java
+package service;
 
+import model.Task;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TaskQueueService {
+    private Queue<Task> taskQueue = new LinkedList<>();
+
+    public void enqueue(Task task) {
+        taskQueue.add(task);
+    }
+
+    public Queue<Task> listTasks() {
+        return taskQueue;
+    }
+
+    public Task dequeue() {
+        return taskQueue.poll();
+    }
+}
